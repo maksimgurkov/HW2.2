dfb736124f0882621b0ba64a7eb6bc7d4399c03f
Maksim Gurkov <maksim.gurkov@gmail.com>
2023-01-30 15:08:23 +0300

    final

--- HW2.2/ViewController.swift
+++ HW2.2/ViewController.swift
@@ -7,13 +7,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum Colors {
+    case red
+    case yellow
+    case green
+}
 
+final class ViewController: UIViewController {
+    
+    // MARK: - IBOutlet
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
+    // MARK: - Private properties
+    private var color = Colors.red
+    private var lightOn: CGFloat = 1
+    private var lightOf: CGFloat = 0.3
+    
+    
+    // MARK: - ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupView()
+        startButton.layer.cornerRadius = 8
     }
-
+    
+    // MARK: - Private Func
+    private func setupView() {
+        redView.alpha = lightOf
+        yellowView.alpha = lightOf
+        greenView.alpha = lightOf
+        redView.layer.cornerRadius = redView.frame.height / 2
+        yellowView.layer.cornerRadius = yellowView.frame.height / 2
+        greenView.layer.cornerRadius = greenView.frame.height / 2
+    }
+    
+    // MARK: - IBAction
+    @IBAction func activeStartButton() {
+        
+        startButton.setTitle("NEXT", for: .normal)
+        
+        switch color {
+        case .red:
+            redView.alpha = lightOn
+            greenView.alpha = lightOf
+            color = .yellow
+        case .yellow:
+            redView.alpha = lightOf
+            yellowView.alpha = lightOn
+            color = .green
+        case .green:
+            yellowView.alpha = lightOf
+            greenView.alpha = lightOn
+            color = .red
+        }
+    }
+    
 
 }
 
